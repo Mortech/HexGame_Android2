56406b8a3287c3d181218bc5776611769bb7086a
William Harmon <xlythe@gmail.com>
2012-02-12 19:07:09 -0500

    Updated icon

--- gen/com/sam/hex/R.java
+++ gen/com/sam/hex/R.java
@@ -8,16 +8,34 @@
 package com.sam.hex;
 
 public final class R {
+    public static final class array {
+        public static final int gameModeArray=0x7f040000;
+        public static final int gameModeValues=0x7f040001;
+        public static final int gameSizeArray=0x7f040002;
+        public static final int gameSizeValues=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+    }
+    public static final class id {
+        public static final int newgame=0x7f060002;
+        public static final int quit=0x7f060003;
+        public static final int settings=0x7f060000;
+        public static final int undo=0x7f060001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int menu=0x7f030000;
+        public static final int preferences=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int inline_preferences=0x7f050005;
+        public static final int newgame=0x7f050003;
+        public static final int quit=0x7f050004;
+        public static final int settings=0x7f050001;
+        public static final int undo=0x7f050002;
     }
 }
