e6e7192ce516ee0e38e636ec00a9857c91dfa07e
Silverdev(Sam) <laane2@illinois.edu>
2012-02-14 19:54:05 -0600

    cleaned up broken classes.

--- gen/com/sam/hex/R.java
+++ gen/com/sam/hex/R.java
@@ -27,8 +27,9 @@ public final class R {
         public static final int undo=0x7f060001;
     }
     public static final class layout {
-        public static final int menu=0x7f030000;
-        public static final int preferences=0x7f030001;
+        public static final int main=0x7f030000;
+        public static final int menu=0x7f030001;
+        public static final int preferences=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
