481c64487cd9ffcbc0179ebc2327d42f2c87f56b
William Harmon <xlythe@gmail.com>
2012-04-16 09:09:44 -0400

    cross compatibility for loading/saving between pc/android

--- src/com/sam/hex/replay/Load.java
+++ src/com/sam/hex/replay/Load.java
@@ -9,6 +9,7 @@ import android.os.Environment;
 import com.sam.hex.Global;
 import com.sam.hex.HexGame;
 import com.sam.hex.MoveList;
+import com.sam.hex.PlayerObject;
 import com.sam.hex.PlayingEntity;
 
 public class Load implements Runnable{
@@ -33,6 +34,14 @@ public class Load implements Runnable{
     				Global.player1Type = (Byte) inputStream.readObject();
     				Global.player2 = (PlayingEntity) inputStream.readObject();
     				Global.player2Type = (Byte) inputStream.readObject();
+    				if(Global.player1==null){
+    					Global.player1=new PlayerObject((byte)1);
+    					Global.player1Type=0;
+    				}
+    				if(Global.player2==null){
+    					Global.player2=new PlayerObject((byte)2);
+    					Global.player2Type=0;
+    				}
     				
     				inputStream.close();
     	        }
